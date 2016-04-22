b7adb6421e7282fffc2bc3ac3d9451f6b3088e89
caulfiel005 <caulfiel005@knights.gannon.edu>
2016-04-22 09:16:32 -0400

    Taken out the bad KnightWatch-master folder

--- KnightWatch-master/Database/knightWatchMainJS3.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("menuButton").onclick = function(){
-    document.getElementById("menuItem").classList.add('leftSlide');
-  }
-
-  document.getElementById("menuClose").onclick = function(){
-    document.getElementById("menuItem").classList.remove("leftSlide");
-  }
